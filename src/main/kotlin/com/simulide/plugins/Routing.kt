1a1bdf4fd82783ed4f4c8733e6abcdc97ed71072
Dalton DeMaria <dalton.demaria@gmail.com>
2024-11-08 18:12:25 -0600

    save endpoint taking the javascript code
    Remaining:
       - save code to the db
       - allow fetching of the code
       - allow code updates via a post endpoint with the snippet addition

--- src/main/kotlin/com/simulide/plugins/Routing.kt
+++ src/main/kotlin/com/simulide/plugins/Routing.kt
@@ -1,23 +1,22 @@
 package com.simulide.plugins
 
 import com.github.mustachejava.DefaultMustacheFactory
+import com.simulide.domain.ExecutableCode
 import io.ktor.http.*
+import io.ktor.http.ContentType.Application.Json
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.mustache.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+import java.util.UUID
 
 data class MustacheUser(val id: Int, val name: String)
-data class LineOfCode(val lineNumber: Int, val tokens: List<String>, val leadingSpaces: String)
-
-val test = """
-    function hello_world() {
-        console.log("hello, world!");
-    }
-    
-    hello_world();
-""".trimIndent()
+data class IndexData(val user: MustacheUser, val csrfToken: String)
 
 fun Application.configureRouting() {
     install(Mustache) {
@@ -28,21 +27,23 @@ fun Application.configureRouting() {
             call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
         }
     }
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
     routing {
         get("/") {
-            call.respond(MustacheContent("htmx/index.hbs", mapOf("user" to MustacheUser(1, "user1"))))
+            val payload = mapOf("data" to IndexData(MustacheUser(1, "user1"), UUID.randomUUID().toString()))
+            call.respond(MustacheContent("htmx/index.hbs", payload))
         }
 
-        get("/code/new") {
-            val lines = test.split("\n")
-            val linesOfCode = lines.mapIndexed { index, line ->
-                val leadingSpace = line.takeWhile { x -> x.isWhitespace() }
-                val split = line.trimStart().split(" ", "\t")
-
-                LineOfCode(index, split, leadingSpace)
-            }
-            val codeContent = mapOf("lines" to linesOfCode)
-            call.respond(MustacheContent("htmx/code.hbs", codeContent))
+        post("/save-code" ) {
+            val payload = call.receive<ExecutableCode>()
+            println(">>>> payload parsed: $payload")
+            call.respond(HttpStatusCode.Created, payload)
         }
     }
 }
