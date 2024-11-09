02e6e9feb0613c6a7f91ce60f6af0ba5aefdfde0
Dalton DeMaria <dalton.demaria@gmail.com>
2024-11-08 18:12:25 -0600

    save endpoint taking the javascript code
    Remaining:
       - save code to the db
       - allow fetching of the code
       - allow code updates via a post endpoint with the snippet addition

--- src/main/kotlin/com/simulide/Application.kt
+++ src/main/kotlin/com/simulide/Application.kt
@@ -9,7 +9,6 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureSockets()
-    configureSerialization()
     configureDatabases()
     configureMonitoring()
     configureHTTP()
